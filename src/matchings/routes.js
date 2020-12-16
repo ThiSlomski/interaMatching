4b05f8a7db7fbf2c0b6c5e66ebd070c0d536d93e
Thiago Slomski <thiagoslomski@hotmail.com>
2020-12-16 16:44:09 -0300

    remove console.log

--- src/matchings/routes.js
+++ src/matchings/routes.js
@@ -6,13 +6,12 @@ const routes = express.Router({
 
 routes.get('/', (req, res) => {
 
-    body = req;
-    console.log(body);
     response = {
         message: "Aqui faremos a lógica do match"
     };
+
     res.status("200").json(response);
-    
+
 });
 
 module.exports= {
